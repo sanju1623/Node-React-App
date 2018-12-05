2f175939ad1141b3ff654c9f2e3ced621d0c5def
sanju1623 <sanjayraghavendra16@gmail.com>
2018-12-05 14:03:39 -0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Node-React-App
-Created with CodeSandbox
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "746396787265-kq1cpchml63noptk0h8bu48aq2c9c3qb.apps.googleusercontent.com",
+  googleClientSecret: "QVHLWhulc0UvHqY3MJCScEHn"
+};
